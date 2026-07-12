70a0f49b25e7feba7f7c41db7a2d8e89870975a5
Brandon Hernández <isahecam@gmail.com>
2026-07-12 16:39:45 -0600

    feat: add email configuration for verification, password reset, and invitations

---
+++ lib/email/email.config.ts
@@ -0,0 +1,8 @@
+export const emailConfig = {
+  from: {
+    verification: "Abarrotia <verification@meeti.isahecam.dev>",
+    passwordReset: "Abarrotia <password-reset@meeti.isahecam.dev>",
+    invitation: "Abarrotia <invitations@meeti.isahecam.dev>",
+  },
+  tokenExpiration: "1 hora",
+};
