0833dab57ba87552e2a9c71e6e4abe3fd5fe1559
Brandon Hernández <isahecam@gmail.com>
2026-07-12 16:49:24 -0600

    feat: add AuthLayout component for authentication pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import { AbarrotiaLogo } from "@/components/composed/layouts/abarrotia-logo";
+
+export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <main className="flex w-full max-w-sm flex-col gap-6">
+        <Link href="/" className="flex items-center gap-2 self-center">
+          <AbarrotiaLogo />
+        </Link>
+        {children}
+        <span className="text-center text-sm text-muted-foreground">
+          2026 Abarrotia. Todos los derechos reservados.
+        </span>
+      </main>
+    </div>
+  );
+}
