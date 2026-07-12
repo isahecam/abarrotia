7276072a602f7cdb2445ebb5e1ed3b698baa27be
Brandon Hernández <isahecam@gmail.com>
2026-07-12 16:48:53 -0600

    feat: add AdminLayout component with sidebar and header structure

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import { AppSidebar } from "@/components/composed/layouts/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min">{children}</div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
