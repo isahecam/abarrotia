8f371fd3de5ab2f87fce4d73927fbdcea794bfaf
Brandon Hernández <isahecam@gmail.com>
2026-07-12 18:35:21 -0600

    feat: add CurrentDateLabel component for displaying the current date

---
+++ components/composed/layouts/current-date-label.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { HTMLAttributes } from "react";
+
+import { cn } from "@/lib/utils";
+import { getFullDate, getIsoDate } from "@/utils/format-date";
+
+export function CurrentDateLabel({ ...props }: Readonly<HTMLAttributes<HTMLTimeElement>>) {
+  const now = new Date();
+
+  const date = getFullDate(now);
+  const isoDate = getIsoDate(now);
+
+  return (
+    <time
+      {...props}
+      dateTime={isoDate}
+      className={cn("px-4 text-sm font-medium text-muted-foreground", props.className)}>
+      {date}
+    </time>
+  );
+}
