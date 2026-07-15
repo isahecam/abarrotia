7800d892c53d0b241dac72604c794528b00ff5ee
Brandon Hernández <isahecam@gmail.com>
2026-07-15 13:53:40 -0600

    feat: implement getAll method in CategoryService for paginated category retrieval

--- features/categories/services/category.service.ts
+++ features/categories/services/category.service.ts
@@ -1,3 +1,4 @@
+import { SearchParams } from "@/app/(dashboard)/categories/search-params";
 import { CategoryError } from "@/features/categories/errors/category-error";
 import { categoryErrorMapper } from "@/features/categories/errors/category-error-mapper";
 import { categoryRepository } from "@/features/categories/repositories/category.repository";
@@ -5,6 +6,7 @@ import { Category } from "@/features/categories/schemas/category.schema";
 import { CategoryRepository, Category as CategorySelect } from "@/features/categories/types/category.types";
 import { err, ok, Result } from "@/lib/errors/result";
 import { SlugGenerator, slugify } from "@/lib/slug";
+import { PaginatedResponse } from "@/types/pagination";
 
 class CategoryService {
   constructor(
@@ -20,6 +22,24 @@ class CategoryService {
     if (error) return err(categoryErrorMapper(error.reason));
 
     return ok(category);
+  }
+
+  async getAll({ page, pageSize }: SearchParams): Promise<Result<CategoryError, PaginatedResponse<CategorySelect>>> {
+    const [error, result] = await this.repository.getAll({ page, pageSize });
+
+    if (error) return err(categoryErrorMapper(error.reason));
+
+    const totalPages = Math.ceil(result.total / pageSize);
+
+    return ok({
+      data: result.data,
+      pagination: {
+        page,
+        pageSize,
+        total: result.total,
+        totalPages,
+      },
+    });
   }
 }
 
