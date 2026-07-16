e87002ab701147dcfdb6c368e1ab38801d801d25
Brandon Hernández <isahecam@gmail.com>
2026-07-15 23:32:45 -0600

    feat: update useFilters to accept options for enhanced query state management

--- app/(dashboard)/categories/search-params.ts
+++ app/(dashboard)/categories/search-params.ts
@@ -1,5 +1,5 @@
 import { useQueryStates } from "nuqs";
-import { createLoader, createSerializer, inferParserType, parseAsInteger, parseAsString } from "nuqs/server";
+import { createLoader, createSerializer, inferParserType, Options, parseAsInteger, parseAsString } from "nuqs/server";
 
 const searchParams = {
   search: parseAsString.withDefault(""),
@@ -12,7 +12,8 @@ export type SearchParams = inferParserType<typeof searchParams>;
 export const loadFilters = createLoader(searchParams);
 export const getPaginatedLink = createSerializer(searchParams);
 
-export const useFilters = () =>
+export const useFilters = (options: Options = {}) =>
   useQueryStates(searchParams, {
+    ...options,
     shallow: false,
   });
