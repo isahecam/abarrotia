048599b83d7e10effba5c217ae07b2d9ac7f2296
Brandon Hernández <isahecam@gmail.com>
2026-07-16 20:40:01 -0600

    feat: add CATEGORY_NOT_FOUND to CATEGORY_ERROR_REASONS

--- features/categories/errors/category-error.ts
+++ features/categories/errors/category-error.ts
@@ -1,4 +1,4 @@
-export const CATEGORY_ERROR_REASONS = ["CATEGORY_ALREADY_EXISTS", "UNEXPECTED_ERROR"] as const;
+export const CATEGORY_ERROR_REASONS = ["CATEGORY_ALREADY_EXISTS", "CATEGORY_NOT_FOUND", "UNEXPECTED_ERROR"] as const;
 
 export type CategoryErrorReason = (typeof CATEGORY_ERROR_REASONS)[number];
 export type CategoryError = { reason: CategoryErrorReason };
