ee750fcd7942ffb83fd94a986e3006e7b1936926
Brandon Hernández <isahecam@gmail.com>
2026-07-18 21:09:56 -0600

    feat: update useDeleteCategory hook to use DeleteCategory type for deletion

--- features/categories/hooks/use-delete-category.ts
+++ features/categories/hooks/use-delete-category.ts
@@ -4,14 +4,14 @@ import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 
 import { deleteCategory } from "@/features/categories/actions/category.actions";
-import { CategoryIdentifier } from "@/features/categories/schemas/category.schema";
+import { DeleteCategory } from "@/features/categories/schemas/category.schema";
 import { appToast } from "@/lib/toast";
 
 export const useDeleteCategory = () => {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
 
-  const onDelete = (id: CategoryIdentifier) => {
+  const onDelete = (id: DeleteCategory) => {
     startTransition(async () => {
       const result = await deleteCategory(id);
 
