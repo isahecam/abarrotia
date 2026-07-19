5d217db3b49501aa6e14f0af8f16094d3ef374aa
Brandon Hernández <isahecam@gmail.com>
2026-07-18 21:51:03 -0600

    feat: add update method to CategoryRepository for updating categories

--- features/categories/types/category.types.ts
+++ features/categories/types/category.types.ts
@@ -9,6 +9,7 @@ export type NewCategory = typeof categories.$inferInsert;
 
 export interface CategoryRepository {
   create(category: NewCategory): Promise<Result<RepositoryError, Category>>;
+  update(id: string, category: Partial<NewCategory>): Promise<Result<RepositoryError, Category>>;
   getAll({ search, page, pageSize }: SearchParams): Promise<Result<RepositoryError, PaginatedResult<Category>>>;
   getById(id: string): Promise<Result<RepositoryError, Category | null>>;
   delete(id: string): Promise<Result<RepositoryError, void>>;
